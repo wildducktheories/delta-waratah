ace1a2eb7a89f70f0e233d027fec2cb806b7aaa6
Jon Seymour <jon@wildducktheories.com>
2021-07-24 14:32:06 +1000

    Split loading related functions into their own module.

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
 from .PhasePlot import *
 from .GrowthPlot import *
 from .functions import *
+from .loaders import *
--- src/functions.py
+++ src/functions.py
@@ -8,39 +8,6 @@ import sys
 from datetime import datetime
 from statsmodels.regression.rolling import RollingOLS
 
-def load_data(fn):
-    df=pd.read_csv(fn).rename({
-        'Date': 'date',
-        'Overseas-acquired': 'overseas',
-        'Overseas acquired': 'overseas',
-        'Known local source': 'local',
-        'Unknown local source (community spread)': 'unknown',
-        'Interstate travel': 'interstate',
-        'Known local source inside HQ': 'local-hq',
-        'Under investigation': 'under-investigation'
-    }, axis=1)
-    try:
-        df["local-hq"] is None
-    except KeyError as e:
-        df["local-hq"] = 0
-    df=calc_cumulative(df)
-    return edit_dates(df)
-
-def load_quarantine(fn):
-    df=pd.read_csv(fn).rename({
-        'Hotel Quarantine': 'hotel-quarantine',
-        'Local Quarantine / Isolation': 'local-quarantine',
-        'No Quarantine / Unspecified': 'no-quarantine',
-        'Not Announced / Under investigation': 'under-investigation'
-    }, axis=1)
-    df['total']=df['under-investigation']+df['no-quarantine']
-    df['cumulative'] = df['total'].cumsum()
-    return edit_dates(df)
-
-def rewrite_date(d):
-    p = d.split('/')
-    return '-'.join(['20'+p[2], p[1], p[0]])
-
 def calc_cumulative(df):
     df['total']=df['local']+df['unknown']+df['under-investigation']+df['local-hq']
     df['cumulative'] = df['total'].cumsum()
@@ -82,39 +49,6 @@ def select_outbreak(df, generation_days=5):
     subset_df["err"] = modeling_errors(subset_df, None)
     return subset_df
 
-def edit_dates(df):
-    newyear=df['date'][df['date']=='01/01'].index.values
-    if len(newyear) > 0:
-        df.loc[df.index < newyear[0],['date']]=df['date'].loc[df.index < newyear[0]]+'/20'
-    else:
-        newyear=[0]
-    df.loc[df.index >= newyear[0],['date']]=df['date'].loc[df.index >= newyear[0]]+'/21'
-    df['date']=df['date'].apply(rewrite_date)
-    return df
-
-def load_vic_data(fn):
-    df=pd.read_csv(fn).rename({
-        'Acquired in Australia, unknown source': 'unknown',
-        'Contact with a confirmed case	': 'local',
-        'Travel overseas': 'overseas'
-    })
-
-    df['count']=1
-    df=df.pivot_table(index=['diagnosis_date'], columns=['acquired'], values=['count'], aggfunc=np.sum)
-
-    tmp=df
-    df=pd.DataFrame(data=df.values, columns=['unknown', 'local', 'overseas'])
-    df[df.loc[:,['unknown']].isna()]=0
-    df[df.loc[:,['local']].isna()]=0
-    df[df.loc[:,['overseas']].isna()]=0
-    df['under-investigation']=0
-
-    df=pd.DataFrame(data=df.values, columns=['unknown', 'local', 'overseas', 'under-investigation'], dtype=int)
-    df['date']=tmp.index
-
-    df['total']=df['local']+df['unknown']+df['under-investigation']
-    df['cumulative'] = df['total'].cumsum()
-    return df
 
 def amnesic_growth(rate=1.5, days=100, window_size=14):
     window = np.zeros(shape=(window_size))
