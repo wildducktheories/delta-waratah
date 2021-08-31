58468ca16812f921dcd5f698824e43914029a36f
Jon Seymour <jon@wildducktheories.com>
2021-08-31 13:21:05 +1000

    Load hospitalisations, icu, ventilated and deaths.

--- src/nswhealth.py
+++ src/nswhealth.py
@@ -48,15 +48,29 @@ def parse_statistics_html(html, debug=False):
 
     html=html.replace("​", "")
     html=html.replace("two", "2")
+    html=html.replace("three", "3")
+    html=html.replace("four", "4")
+    html=html.replace("Four", "4")
     html=html.replace("five", "5")
-    html=html.replace("no", "0")
+    html=html.replace("six", "6")
+    html=html.replace("seven", "7")
+    html=html.replace(" no ", " 0 ")
 
     pattern_cumulative=re.compile("^.*There have been ([\d,]*) locally acquired cases reported since 16 June 2021")
-    pattern=re.compile("^.*<p[^>]*>[. ]*NSW recorded ([^ ]*) .*locally acquired cases of COVID-19")
+    pattern=re.compile("^.*NSW recorded ([^ ]*) .*locally acquired cases of COVID-19")
     pattern_partial=re.compile("^.*NSW recorded")
-
+    pattern_health=re.compile(".*There are currently ([\d,]*) COVID-19 cases admitted to hospital, with ([\d,]*) people in intensive care, (.*) of whom require ventilation.")
+    pattern_deaths=re.compile(".*There have been ([\d,]*) COVID.*related deaths")
+    pattern_deaths_2=re.compile(".*This brings the number of COVID-related deaths to ([\d,]*)")
+    pattern_deaths_3=re.compile(".*This brings to ([\d,]*) the number of COVID-related deaths")
+    pattern_deaths_4=re.compile(".*The number of COVID-related deaths during the current outbreak is now ([\d,]*)")
     total=None
     cumulative=None
+    hospitalised=None
+    icu=None
+    ventilated=None
+    deaths=None
+
     for c in html.split("\n"):
         g=pattern_cumulative.match(c)
         if g:
@@ -65,19 +79,38 @@ def parse_statistics_html(html, debug=False):
             g=pattern.match(c)
             if g:
                 total=int(g.groups()[0].replace(",",""))
-                continue
             else: 
                 if debug:
                     print(c)
-    return (total, cumulative)
+        g=pattern_health.match(c)
+        if g:
+            hospitalised,icu,ventilated=[int(v.replace(',', '')) for v in g.groups()]
+
+        g=pattern_deaths.match(c)
+        if g:
+            deaths=int(g.groups()[0].replace(",", ""))
+
+        g=pattern_deaths_2.match(c)
+        if g:
+            deaths=int(g.groups()[0].replace(",", ""))
+
+        g=pattern_deaths_3.match(c)
+        if g:
+            deaths=int(g.groups()[0].replace(",", ""))
+
+        g=pattern_deaths_4.match(c)
+        if g:
+            deaths=int(g.groups()[0].replace(",", ""))
+
+    return (total, cumulative, hospitalised, icu, ventilated, deaths)
 
 def load_nswhealth_stats():
     out=[]
     index=load_index()
     for date in index:
-        (total, cumulative)=parse_statistics_html(load_statistics(index[date], date))
-        out.append((date, total, cumulative))
-    df = pd.DataFrame(columns=['date', 'total', 'cumulative_corrected'], data=out)
+        (total, cumulative, hospitalised, icu, ventilated, deaths)=parse_statistics_html(load_statistics(index[date], date))
+        out.append((date, total, cumulative, hospitalised, icu, ventilated, deaths))
+    df = pd.DataFrame(columns=['date', 'total',  'cumulative_corrected', 'hospitalised', 'icu', 'ventilated', 'deaths'], data=out)
     df['cumulative'] = df['total'].cumsum()
     df['correction'] = df['cumulative_corrected'] - df['cumulative']
     return df
